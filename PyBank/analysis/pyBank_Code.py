a932f906fa2d702794927d153375cc7761eaead7
m-rivera1 <mike.11.mxr@gmail.com>
2020-10-20 11:14:53 -0500

    updated main.py to use dict instead of list

--- PyBank/analysis/pyBank_Code.py
+++ PyBank/analysis/pyBank_Code.py
@@ -12,53 +12,57 @@ os.chdir("C:\\Users\\mike1\\Downloads")
 
 budget_csv = "Budget_data.csv"
 
+# establish empy dictionary
+profit_loss = {} 
 
-profit_loss = [] 
-months_list = []
 
 # Read in the CSV file
-    
 with open(budget_csv, 'r') as csvfile:
     
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
     
     header = next(csvreader)
-        
+    # loop to add data to profit_loss dict    
     for row in csvreader:
-        profit_loss.append(int(row[1]))
-        months_list.append(row[0])       
-
-             
-
+        profit_loss.update({row[0]: int(row[1])})
+        
+  
 
 
-
+#define function to calculate and print
 def print_analysis (profit_loss):
     
-    
-    total_months = len(profit_loss)
-    monthly_total = sum(profit_loss)
-    average = monthly_total/len(profit_loss)
-    greatest_increase = max(profit_loss)
-    greatest_decrease = min(profit_loss)
-    
-    #get index of greatest increase and decrease
-    #then grab the date with same index number
-    #from the months_list
+    #calculations
+    total_months = len(profit_loss.values())
+    monthly_total = sum(profit_loss.values())
+    average = monthly_total/len(profit_loss.values())
+    greatest_increase = max(profit_loss.values())
+    greatest_decrease = min(profit_loss.values())
 
-    print("Financial Analysis")
-    print("-------------------------")
-    print(f' Total Months: {total_months}')  
-    print(f' Total: ${monthly_total}')
-    print(f' Average Change: ${average}')
-    print(f' Greatest Increase in Profits:  ${greatest_increase}')
-    print(f' Greatest Decrease in Profits:  ${greatest_decrease}')
-
+    #loop to find the corresponding keys for the greatest
+    #increase and decrease
+    for key, value in profit_loss.items():
+        if value == greatest_decrease:
+           de_date = key
+        if value == greatest_increase:
+            in_date = key
+    
+        
+    
+ 
+    #print table
+    print("              Financial Analysis")
+    print("------------------------------------------------")
+    print(f' Total Months:                 {total_months}')  
+    print(f' Total:                        ${monthly_total}')
+    print(f' Average Change:               ${round(average,2)}')
+    print(f' Greatest Increase in Profits: {in_date} ${greatest_increase}')
+    print(f' Greatest Decrease in Profits: {de_date} ${greatest_decrease}')
 
  
    
-
+#call function to run analysis and print
 print_analysis(profit_loss)
    
    
